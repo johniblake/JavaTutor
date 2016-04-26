c065aa1cb16bd14ad830bb5f04866ac9134e9f03
John Blake <blakej@carleton.edu>
2016-04-25 23:59:31 -0500

    ~

--- example_code/java/example_06/ArraySum.java
+++ example_code/java/example_06/ArraySum.java
@@ -6,11 +6,10 @@ public class ArraySum{
         //instantiate an ArrayList called "array"
         ArrayList<Integer> array = new ArrayList<Integer>();
         int k = 0;
-        //populate array with 10 1's
+        
         for (int i = 0; i < 10; i++){
           array.add(1);
         }
-        //add each item to the value stored in k
         for (int j = 0; j < 10; j++){
           k += array.get(j);
         }
